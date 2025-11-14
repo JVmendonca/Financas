6b0d7f2fc9f7b13630a3dbed155828a7de0f2ab1
JVmendonca <craftgame157@gmail.com>
2025-11-13 21:20:32 -0300

    test: add RegisterDispensasValidator tests for title/date/value rules
    - Add success case
    - Fail when title is empty
    - Fail when date is in the future
    - Fail when value <= 0 (0, -1, -2, -3)
    - Assert expected resource error messages

--- tests/Validator.Tests/Dispesas/Register/RegisterDispensasValidatorTest.cs
+++ tests/Validator.Tests/Dispesas/Register/RegisterDispensasValidatorTest.cs
@@ -1,7 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-using CommonTestUtilities;
+﻿using CommonTestUtilities;
 using Financas.Application.UseCases.Dispesas.Register;
-using Financas.Communication.Request;
+using Financas.Exeption;
 using FluentAssertions;
 namespace Validator.Tests.Dispesas.Register;
 public class RegisterDispensasValidatorTest
@@ -19,5 +18,61 @@ public class RegisterDispensasValidatorTest
         // Valida
         reuslt.IsValid.Should().BeTrue();
     }
-    
+
+    [Fact]
+    public void Error_Titulo_Obrigatorio()
+    {
+        // Preparar
+        var validator = new RegisterDispensasValidator();
+        var request = RequestDispesaJsonBuilder.Build();
+        request.Titulo = string.Empty;
+        //request.Valor = 0;
+
+        // Agir
+        var reuslt = validator.Validate(request);
+
+        // Valida
+        reuslt.IsValid.Should().BeFalse();
+        reuslt.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMassages.TITULO_OBRIGATORIO));
+        
+    }
+
+    [Fact]
+    public void Error_Data_Futura()
+    {
+        // Preparar
+        var validator = new RegisterDispensasValidator();
+        var request = RequestDispesaJsonBuilder.Build();
+        request.Data = DateTime.UtcNow.AddDays(1);
+        //request.Valor = 0;
+
+        // Agir
+        var reuslt = validator.Validate(request);
+
+        // Valida
+        reuslt.IsValid.Should().BeFalse();
+        reuslt.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMassages.DATA_NAO_DEVE_SER_FUTURA));
+        
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-3)]
+    public void Error_Valor_Invalido(decimal valor)
+    {
+        // Preparar
+        var validator = new RegisterDispensasValidator();
+        var request = RequestDispesaJsonBuilder.Build();
+        request.Valor = valor;
+        // Agir
+        var reuslt = validator.Validate(request);
+
+        // Valida
+        reuslt.IsValid.Should().BeFalse();
+        reuslt.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMassages.VALOR_DEVE_SER_MAIOR_ZERO));
+        
+    }
+
 }
