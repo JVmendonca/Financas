3811887f50be65635a4c7a68ca271e00b505811e
JVmendonca <craftgame157@gmail.com>
2025-12-09 23:59:14 -0300

    Add localization resources for report generation
    Added .resx and auto-generated designer files for report error and generation messages in English and Portuguese. Updated Financas.Domain.csproj to include these resources. Enhanced GenereteDespesaReportExcelUseCase to use localized headers in Excel reports, improving internationalization support.

--- src/Financas.Application/UseCases/Dispesas/Reports/Excel/GenereteDespesaReportExcelUseCase.cs
+++ src/Financas.Application/UseCases/Dispesas/Reports/Excel/GenereteDespesaReportExcelUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Spreadsheet;
+using Financas.Domain.Reports;
 
 namespace Financas.Application.UseCases.Dispesas.Reports.Excel;
 public class GenereteDespesaReportExcelUseCase : IGenereteDespesaReportExcelUseCase
@@ -13,5 +14,27 @@ public class GenereteDespesaReportExcelUseCase : IGenereteDespesaReportExcelUseC
         workbook.Style.Font.FontName = "Times New Roman";
 
         var worksheet = workbook.AddWorksheet(mes.ToString("MM-yyyy"));
+
+        InsertHeader(worksheet);
     }
+
+    private void InsertHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = ResourcereportGenerationMessages.TITULO;
+        worksheet.Cell("B1").Value = ResourcereportGenerationMessages.DATA;
+        worksheet.Cell("C1").Value = ResourcereportGenerationMessages.PAGAMENTO;
+        worksheet.Cell("D1").Value = ResourcereportGenerationMessages.VALOR;
+        worksheet.Cell("E1").Value = ResourcereportGenerationMessages.DESCRICAO;
+
+        worksheet.Cells("A1:E1").Style.Font.Bold = true;
+
+        worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");
+
+        worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+    }
+
 }
